65f0d2d6882a1f4a47d565c84fca816f43a5605f
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-05-08 20:10:08 +0400

    :alembic: Replicate current view with Elm UI

--- src/Main.elm
+++ src/Main.elm
@@ -1,6 +1,12 @@
 module Main exposing (..)
 
 import Browser
+import Debug exposing (toString)
+import Element exposing (Color, alignLeft, layout)
+import Element.Background
+import Element.Border
+import Element.Font
+import Element.Input
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (..)
@@ -130,10 +136,14 @@ update msg { balance, gameState } =
                 newModel =
                     case gameResult of
                         MarkWins amount ->
-                            { balance = balance + amount, gameState = Resolved rollResults gameResult }
+                            { balance = balance + amount
+                            , gameState = Resolved rollResults gameResult
+                            }
 
                         ZaraWins amount ->
-                            { balance = balance - amount, gameState = Resolved rollResults gameResult }
+                            { balance = balance - amount
+                            , gameState = Resolved rollResults gameResult
+                            }
             in
             ( newModel, Cmd.none )
 
@@ -169,8 +179,56 @@ dieGenerator =
 -- VIEW
 
 
-view : Model -> Html Msg
-view model =
+view2 : Model -> Html Msg
+view2 { balance, gameState } =
+    let
+        balanceDisplay =
+            Element.text ("Balance: " ++ toString balance)
+
+        contextInfo =
+            Element.row [ Element.alignRight ]
+                [ balanceDisplay
+                ]
+
+        rollResultsDisplay =
+            case gameState of
+                Staked _ ->
+                    Element.text "Rolling..."
+
+                Resolved ( rolledA, rolledB ) _ ->
+                    Element.row
+                        [ Element.Font.size 200
+                        ]
+                        [ Element.text (viewDieFace rolledA)
+                        , Element.text (viewDieFace rolledB)
+                        ]
+
+        controlPanel =
+            Element.row
+                [ Element.centerX
+                , Element.Border.width 2
+                , Element.Border.rounded 2
+                , Element.padding 8
+                ]
+                [ rollTrigger ]
+
+        rollTrigger =
+            Element.Input.button []
+                { label = Element.text "Roll for 1000"
+                , onPress = Just (Bet 1000)
+                }
+    in
+    layout []
+        (Element.column []
+            [ contextInfo
+            , rollResultsDisplay
+            , controlPanel
+            ]
+        )
+
+
+view1 : Model -> Html Msg
+view1 model =
     div []
         [ button [ onClick (Bet 1000) ] [ text "Spin" ]
         , case model.gameState of
@@ -184,6 +242,15 @@ view model =
         , div []
             [ text ("Mark: " ++ String.fromInt model.balance)
             ]
+        ]
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ view1 model
+        , hr [] []
+        , view2 model
         ]
 
 
