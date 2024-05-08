f38f12dbb48d3853f40332890c1de431187912b3
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-05-09 00:47:42 +0400

    :soap: retouch benzino tests for clarity

--- tests/BenzinoTests.elm
+++ tests/BenzinoTests.elm
@@ -1,8 +1,7 @@
 module BenzinoTests exposing (..)
 
 import Expect exposing (..)
-import Main exposing (Accounts, DieFace(..), GameResult(..), GameState(..), Model, Msg(..), RollResult, evaluateGameResult, update)
-import Random
+import Main exposing (DieFace(..), GameResult(..), GameState(..), Msg(..), evaluateGameResult, update)
 import Test exposing (..)
 import Time exposing (Weekday(..))
 
@@ -13,120 +12,50 @@ updateTests =
         [ test "When a bet is placed" <|
             \() ->
                 let
-                    defaultGameState : GameState
-                    defaultGameState =
+                    initialGameState : GameState
+                    initialGameState =
                         Resolved ( Yek, Du ) (MarkWins 0)
-
-                    initialModel : Model
-                    initialModel =
-                        { balance = 3000, gameState = defaultGameState }
                 in
-                update (Bet 1000) initialModel
+                { balance = 3000, gameState = initialGameState }
+                    |> update (Bet 1000)
                     |> Tuple.first
                     |> Expect.equal { balance = 2000, gameState = Staked 1000 }
         , describe "When the game resolves"
             [ test "And player wins" <|
                 \() ->
                     let
-                        initialState : Model
-                        initialState =
-                            { balance = 2000, gameState = Staked 1000 }
-
-                        incomingCombination =
+                        winningCombination =
                             ( Yek, Yek )
-
-                        expectedGameState : GameState
-                        expectedGameState =
-                            Resolved incomingCombination (MarkWins 6000)
                     in
-                    update (GameResolves incomingCombination) initialState
+                    { balance = 2000, gameState = Staked 1000 }
+                        |> update (GameResolves winningCombination)
                         |> Tuple.first
-                        |> Expect.equal { balance = 8000, gameState = expectedGameState }
+                        |> Expect.equal
+                            { balance = 8000
+                            , gameState = Resolved winningCombination (MarkWins 6000)
+                            }
             , test "And player loses" <|
                 \() ->
                     let
-                        initialState : Model
-                        initialState =
-                            { balance = 2000, gameState = Staked 1000 }
-
-                        incomingCombination =
+                        losingRoll =
                             ( Yek, Du )
-
-                        expectedGameState : GameState
-                        expectedGameState =
-                            Resolved incomingCombination (ZaraWins 1000)
                     in
-                    update (GameResolves incomingCombination) initialState
+                    { balance = 2000, gameState = Staked 1000 }
+                        |> update (GameResolves losingRoll)
                         |> Tuple.first
-                        |> Expect.equal { balance = 2000, gameState = expectedGameState }
+                        |> Expect.equal
+                            { balance = 2000
+                            , gameState = Resolved losingRoll (ZaraWins 1000)
+                            }
             ]
         ]
 
 
-
---    PlayerBets _ ->
---         ( model
---         , Random.generate GameResolves dieRoller
---         )
---     GameResolves rollResults ->
---         ( rollResults
---         , Cmd.none
---         )
---     DetermineWin _ ->
---         ( model
---         , Cmd.none
---         )
-
-
-dieRoller : Random.Generator RollResult
-dieRoller =
-    Random.pair dieGenerator dieGenerator
-
-
-dieGenerator : Random.Generator DieFace
-dieGenerator =
-    Random.uniform Yek
-        [ Du
-        , Se
-        , Jhar
-        , Panj
-        , Shesh
-        ]
-
-
-juxtZarTests1 : Test
-juxtZarTests1 =
+gameRulesTests : Test
+gameRulesTests =
     describe "Player Wins"
         [ test "When both dice are the same" <|
             \() ->
                 evaluateGameResult 1000 ( Yek, Yek )
                     |> Expect.equal (MarkWins 6000)
-        , test "Then Zara pays" <|
-            \() ->
-                let
-                    accounts : Accounts
-                    accounts =
-                        { mark = 1000, zara = 50000 }
-
-                    rollWinningCombination : () -> RollResult
-                    rollWinningCombination () =
-                        ( Yek, Yek )
-
-                    settle : Accounts -> GameResult -> Accounts
-                    settle initialAccounts result =
-                        case result of
-                            MarkWins amount ->
-                                { mark = initialAccounts.mark + amount
-                                , zara = initialAccounts.zara - amount
-                                }
-
-                            ZaraWins amount ->
-                                { mark = initialAccounts.mark - amount
-                                , zara = initialAccounts.zara + amount
-                                }
-                in
-                rollWinningCombination ()
-                    |> evaluateGameResult 1000
-                    |> settle accounts
-                    |> Expect.equal { mark = 7000, zara = 44000 }
         ]
