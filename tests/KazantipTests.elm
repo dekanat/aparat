96a5da6eed81494493cec37f35f512484074a29e
darthspectrum <muradianator@gmail.com>
2024-05-10 22:21:52 +0000

    :alembic: compose together

---
+++ tests/KazantipTests.elm
@@ -0,0 +1,14 @@
+module KazantipTests exposing (..)
+
+import Aparat exposing (Bet(..), Payout(..))
+import Balance exposing (Balance(..))
+import Expect exposing (..)
+import Player exposing (..)
+import Test exposing (..)
+
+
+compoundTest : Test
+compoundTest =
+    describe "Round"
+        [ todo "single betting cycle - from balance to balance"
+        ]
