8a6c44ff8619da8b751366be022a3d48790df98b
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-05-11 22:10:35 +0400

    :construction:

---
+++ src/Account.elm
@@ -0,0 +1,36 @@
+module Account exposing (..)
+
+import Common.Money exposing (Money)
+
+
+type Account
+    = Account Money
+
+
+add : Money -> Account -> Account
+add amount account =
+    case account of
+        Account current ->
+            Account (current + amount)
+
+
+type AccountingProblem
+    = InsufficientBalance
+
+
+deduct : Money -> Account -> Result AccountingProblem Account
+deduct amount account =
+    case account of
+        Account balance ->
+            if balance >= amount then
+                Ok (Account (balance - amount))
+
+            else
+                Err InsufficientBalance
+
+
+hasAtLeast : Money -> Account -> Bool
+hasAtLeast amount account =
+    case account of
+        Account balance ->
+            balance >= amount
