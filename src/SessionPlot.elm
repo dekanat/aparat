bc4304f2bc78a4a179192f23ba3ed4b298ca34ac
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-05-15 19:13:13 +0400

    :black_cat: return win/lose identificators

--- src/SessionPlot.elm
+++ src/SessionPlot.elm
@@ -2,15 +2,21 @@ module SessionPlot exposing (..)
 
 import Chart as C
 import Chart.Attributes as CA
-import Common.Money exposing (Money)
+import Common.Helpers exposing (slidingWindow)
 import Element exposing (..)
 import Session exposing (SessionState)
 
 
-type alias BalancePlotPoint =
-    { idx : Int
-    , balance : Money
+plotBalancePoint : Int -> Int -> { idx : Float, balance : Float }
+plotBalancePoint idx balance =
+    { idx = toFloat idx
+    , balance = toFloat balance
     }
+
+
+type PlotElement
+    = BalanceLine { idx : Float, balance : Float }
+    | ResultIdentificator { idx : Float, isWin : Bool }
 
 
 plotSession : SessionState e -> Element msg
@@ -19,6 +25,42 @@ plotSession currentState =
         balanceFlow =
             currentState
                 |> Session.balanceSettledThrough
+
+        plottedBalanceFlow =
+            balanceFlow
+                |> List.indexedMap plotBalancePoint
+
+        plottedWinLose =
+            plottedBalanceFlow
+                |> slidingWindow 2
+                |> List.foldl
+                    (\window acc ->
+                        case window of
+                            [ a, b ] ->
+                                { idx = a.idx + (b.idx - a.idx) / 2
+                                , isWin = b.balance > a.balance
+                                }
+                                    :: acc
+
+                            _ ->
+                                acc
+                    )
+                    []
+
+        scatterIdentificators =
+            C.seriesMap ResultIdentificator
+                .idx
+                [ C.scatter (\_ -> 0) []
+                    |> C.variation
+                        (\_ w ->
+                            if w.isWin then
+                                [ CA.plus, CA.color CA.mint ]
+
+                            else
+                                [ CA.cross, CA.color CA.pink ]
+                        )
+                ]
+                plottedWinLose
 
         optimalWidth =
             List.length balanceFlow * 20
@@ -29,12 +71,14 @@ plotSession currentState =
                 , CA.width (toFloat optimalWidth)
                 ]
                 [ C.yLabels [ CA.amount 1, CA.withGrid ]
-                , C.series (.idx >> toFloat)
-                    [ C.interpolated (.balance >> toFloat)
+                , C.seriesMap BalanceLine
+                    .idx
+                    [ C.interpolated .balance
                         [ CA.monotone, CA.width 2 ]
                         [ CA.circle ]
                     ]
-                    (balanceFlow |> List.indexedMap BalancePlotPoint)
+                    plottedBalanceFlow
+                , scatterIdentificators
                 ]
     in
     Element.el
