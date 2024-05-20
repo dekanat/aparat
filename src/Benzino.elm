f38fbef2db062fd40dca1a1f5a8910147d83f14b
darthspectrum <muradianator@gmail.com>
2024-05-20 22:22:04 +0000

    Co-authored-by: antaranyan <antaranyan@users.noreply.github.com>

--- src/Benzino.elm
+++ src/Benzino.elm
@@ -2,7 +2,10 @@ module Benzino exposing (..)
 
 import Account exposing (Account(..))
 import Aparat exposing (DiceRoll)
+import Common.Die exposing (Face(..), glyphFor)
 import Common.Money exposing (Money)
+import Element
+import Element.Font
 import Random
 
 
@@ -26,3 +29,60 @@ playRound bet seed =
                 , payout = Aparat.calculatePayout bet event
                 }
             )
+
+
+type Msg
+    = BetPlaced Money
+    | DiceRolled
+    | RoundResolved
+
+
+type alias Model =
+    { seed : Random.Seed
+    , event : DiceRoll
+    , bet : Money
+    }
+
+
+type Effect
+    = Payout Money
+
+
+update : Msg -> Model -> ( Model, List Effect )
+update msg model =
+    case msg of
+        BetPlaced bet ->
+            ( { model | bet = bet }, [] )
+
+        DiceRolled ->
+            let
+                ( event, seed ) =
+                    model.seed |> Random.step Aparat.rollingPairOfDice
+            in
+            ( { model | event = event, seed = seed }, [] )
+
+        RoundResolved ->
+            let
+                money =
+                    Aparat.calculatePayout model.bet model.event
+            in
+            ( model, [ Payout money ] )
+
+
+view : Model -> Element.Element Msg
+view { event } =
+    let
+        xxlSize =
+            200
+
+        pictogramFor : ( Face, Face ) -> Element.Element Msg
+        pictogramFor ( rolledA, rolledB ) =
+            Element.row
+                [ Element.Font.size xxlSize
+                , Element.spacing 8
+                ]
+                [ Element.text (glyphFor rolledA)
+                , Element.text (glyphFor rolledB)
+                ]
+    in
+    pictogramFor event
