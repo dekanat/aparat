efa9dfe489802ac6892792254de7e7b20b8e6b73
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-05-24 00:30:19 +0400

    :golf: test essential accounting requests

--- tests/AccountingTests.elm
+++ tests/AccountingTests.elm
@@ -1,17 +1,58 @@
 module AccountingTests exposing (..)
 
 import Accounting.Account as Account exposing (Account(..))
+import Accounting.Accounting exposing (Request(..), updateWith)
+import Common.Money exposing (Money)
 import Expect exposing (..)
 import Test exposing (..)
+
+
+balanceOf : Account -> Money
+balanceOf (Account balance) =
+    balance
 
 
 accountingTests : Test
 accountingTests =
     describe "Accounting"
-        [ describe "Recieving a withdrawal request"
-            [ todo "when the balance allows it"
-            , todo "when the balance does not allow it"
+        [ describe "Withdraw"
+            [ test "fulfills when the balance allows it" <|
+                \() ->
+                    Account 1200
+                        |> updateWith
+                            { fulfillOrder = always Expect.pass
+                            , rejectOrder = Expect.fail "But balance was sufficient"
+                            }
+                            (Withdraw 1000)
+                        |> Expect.all
+                            [ Tuple.first >> balanceOf >> Expect.equal 200
+                            , Tuple.second >> Maybe.withDefault (Expect.fail "Should yield")
+                            ]
+            , test "rejects when the balance does not allows it" <|
+                \() ->
+                    Account 800
+                        |> updateWith
+                            { fulfillOrder = always (Expect.fail "But balance was sufficient")
+                            , rejectOrder = Expect.pass
+                            }
+                            (Withdraw 1000)
+                        |> Expect.all
+                            [ Tuple.first >> balanceOf >> Expect.equal 800
+                            , Tuple.second >> Maybe.withDefault (Expect.fail "Should yield")
+                            ]
             ]
+        , test "Replenish" <|
+            \() ->
+                Account 800
+                    |> updateWith
+                        { fulfillOrder = always (Expect.fail "Nothing to yield")
+                        , rejectOrder = Expect.fail "Nothing to yield"
+                        }
+                        (Replenish 200)
+                    |> Expect.all
+                        [ Tuple.first >> balanceOf >> Expect.equal 1000
+                        , Tuple.second >> Maybe.withDefault Expect.pass
+                        ]
         ]
 
 
