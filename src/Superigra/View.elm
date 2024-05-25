a4ca665f660fe3d4bd76f4bac704bade7f46e733
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-05-25 21:50:38 +0400

    chore: drawing the card deck

--- src/Superigra/View.elm
+++ src/Superigra/View.elm
@@ -1,7 +1,22 @@
 module Superigra.View exposing (..)
 
 import Element exposing (text)
+import Element.Font
+import List.Extra
+import Superigra.Card as Card
+import Superigra.Deck
 
 
 view =
-    Element.el [] (text "ok")
+    Element.column [ Element.spacing 15 ]
+        (Superigra.Deck.deck
+            |> List.map
+                (Card.cardToUnicode >> Element.text)
+            |> List.Extra.groupsOf 13
+            |> List.map
+                (Element.row
+                    [ Element.spacing 8
+                    , Element.Font.size 27
+                    ]
+                )
+        )
