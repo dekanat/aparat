1cb968863db1f2253789c01ba8487099cd6ab688
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-05-25 22:35:40 +0400

    :construction:

--- src/Superigra/View.elm
+++ src/Superigra/View.elm
@@ -2,21 +2,49 @@ module Superigra.View exposing (..)
 
 import Element exposing (text)
 import Element.Font
-import List.Extra
-import Superigra.Card as Card
-import Superigra.Deck
+import Random exposing (..)
+import Superigra.Card exposing (..)
+import Superigra.Deck as Deck
+
+
+cardBack =
+    "🂠"
+
+
+unrevealedCard =
+    Element.text cardBack
 
 
 view =
-    Element.column [ Element.spacing 15 ]
-        (Superigra.Deck.deck
-            |> List.map
-                (Card.cardToUnicode >> Element.text)
-            |> List.Extra.groupsOf 13
-            |> List.map
-                (Element.row
-                    [ Element.spacing 8
-                    , Element.Font.size 27
-                    ]
-                )
-        )
+    let
+        dealtCards =
+            Deck.deck |> List.take 5
+
+        cards =
+            case dealtCards of
+                open :: closed ->
+                    { card = open, revealed = True }
+                        :: (closed |> List.map (\cc -> { card = cc, revealed = False }))
+
+                _ ->
+                    []
+
+        cardsOnTable =
+            cards |> List.map cardElement
+    in
+    Element.row [ Element.spacing 15, Element.Font.size 72 ]
+        cardsOnTable
+
+
+type alias CardInTheGame =
+    { card : Card
+    , revealed : Bool
+    }
+
+
+cardElement { card, revealed } =
+    if revealed then
+        card |> cardToUnicode |> text
+
+    else
+        cardBack |> text
