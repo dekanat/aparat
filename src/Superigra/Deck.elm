20ee0f2c3ecd11244ae4a63de49ecbfd4cc0f272
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-05-25 21:33:04 +0400

    :construction:

---
+++ src/Superigra/Deck.elm
@@ -0,0 +1,39 @@
+module Superigra.Deck exposing (..)
+
+import Expect
+import List.Extra
+import Superigra.Card as Card exposing (Card)
+import Test exposing (Test, describe, test)
+
+
+type alias Deck =
+    List Card
+
+
+deck : Deck
+deck =
+    Card.suits
+        |> List.Extra.andThen
+            (\suit ->
+                Card.ranks
+                    |> List.Extra.andThen (\rank -> [ Card rank suit ])
+            )
+
+
+deckTests : Test
+deckTests =
+    describe "Deck"
+        [ test """ Should be standard 52-card deck
+
+            Spades:   🂢 🂣 🂤 🂥 🂦 🂧 🂨 🂩 🂪 🂫 🂭 🂮 🂡
+            Hearts:   🂲 🂳 🂴 🂵 🂶 🂷 🂸 🂹 🂺 🂻 🂽 🂾 🂱
+            Diamonds: 🃂 🃃 🃄 🃅 🃆 🃇 🃈 🃉 🃊 🃋 🃍 🃎 🃁
+            Clubs:    🃒 🃓 🃔 🃕 🃖 🃗 🃘 🃙 🃚 🃛 🃝 🃞 🃑
+        """ <|
+            \() ->
+                deck
+                    |> Expect.all
+                        [ List.length >> Expect.equal 52
+                        , List.Extra.unique >> List.length >> Expect.equal 52
+                        ]
+        ]
