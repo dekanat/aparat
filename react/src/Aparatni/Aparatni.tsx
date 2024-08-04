76ef4feb13e4998f187f82d7b1c500ff3af70850
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-08-04 22:03:25 +0400

    :cat2:

--- react/src/Aparatni/Aparatni.tsx
+++ react/src/Aparatni/Aparatni.tsx
@@ -11,7 +11,7 @@ const allCards = [
   'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/3/3a/TheLovers.jpg',
+  'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/RWS_Tarot_06_Lovers.jpg/640px-RWS_Tarot_06_Lovers.jpg',
   'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
 ]
 
