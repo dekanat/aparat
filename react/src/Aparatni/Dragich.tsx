a4d89f419eb1979329f5c63f8ae78adb13e49c5e
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-08-05 17:56:44 +0400

    :construction:

--- react/src/Aparatni/Dragich.tsx
+++ react/src/Aparatni/Dragich.tsx
@@ -1,18 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import styles from './style.module.css'
+import styles from "./style.module.css";
+import { Vector2, useDrag } from "@use-gesture/react";
 
 const allCards = [
-  'https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/RWS_Tarot_06_Lovers.jpg/640px-RWS_Tarot_06_Lovers.jpg',
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg',
-]
+  "https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/RWS_Tarot_06_Lovers.jpg/640px-RWS_Tarot_06_Lovers.jpg",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg",
+];
 
 type CardProps = {
-  url: string
-}
+  url: string;
+};
 
 function Card({ url }: CardProps) {
   return (
@@ -21,32 +22,56 @@ function Card({ url }: CardProps) {
         <img src={url} />
       </div>
     </>
-  )
+  );
 }
 
-const positionOf = (i:number) => {
-  const dx = 10
-  const dy = 10
+const positionOf = (i: number) => {
+  const dx = 10;
+  const dy = 10;
 
   return {
     top: i * dx,
-    left: i * dy
-  }
-}
+    left: i * dy,
+  };
+};
 
 function Dragich() {
+  const options = {
+    // axis: "y" as const,
+  };
+
+  const cardDrags = useDrag((state) => {
+    const atRestWhen = ([vx, vy]: Vector2) => vx === 0 && vy === 0;
+
+    if (state.active && state.first) {
+      console.log("Drag started");
+    } else if (state.active) {
+      if (atRestWhen(state.velocity)) {
+        console.log("Dragging paused");
+      } else {
+        console.log(`Dragging continues at ${state.velocity} px/ms`);
+      }
+    } else {
+      console.log("Drag ended");
+    }
+
+    console.log(state);
+  }, options);
 
   return (
     <>
       {allCards.map((url, i) => (
         <div className={styles.deck} key={i}>
-          <div style={{position: 'absolute', ...positionOf(i) }}>
+          <div
+            {...cardDrags()}
+            style={{ position: "absolute", ...positionOf(i) }}
+          >
             <Card url={url} />
           </div>
         </div>
       ))}
     </>
-  )
+  );
 }
 
 export default function App() {
@@ -54,5 +79,5 @@ export default function App() {
     <div className={`flex fill center ${styles.container}`}>
       <Dragich />
     </div>
-  )
+  );
 }
