9ae12d66c36c1c76dccbf36148e1b8f761df5be4
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2024-08-07 16:50:23 +0400

    :construction:
    :reverse: :reverse:

--- react/src/Aparatni/Dragich.tsx
+++ react/src/Aparatni/Dragich.tsx
@@ -7,8 +7,8 @@ const allCards = [
   "https://upload.wikimedia.org/wikipedia/commons/f/f5/RWS_Tarot_08_Strength.jpg",
   "https://upload.wikimedia.org/wikipedia/commons/5/53/RWS_Tarot_16_Tower.jpg",
   "https://upload.wikimedia.org/wikipedia/commons/9/9b/RWS_Tarot_07_Chariot.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/RWS_Tarot_06_Lovers.jpg/640px-RWS_Tarot_06_Lovers.jpg",
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg",
+  // "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/RWS_Tarot_06_Lovers.jpg/640px-RWS_Tarot_06_Lovers.jpg",
+  // "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/RWS_Tarot_02_High_Priestess.jpg/690px-RWS_Tarot_02_High_Priestess.jpg",
 ];
 
 type CardProps = {
@@ -35,10 +35,27 @@ const positionOf = (i: number) => {
   };
 };
 
+type TarotCard = {
+  url: string;
+  position: {
+    top: number;
+    left: number;
+  };
+};
+
 function Dragich() {
   const options = {
-    // axis: "y" as const,
+    axis: "y" as const,
   };
+
+  const initialTarotCards = allCards.map((url, i) => {
+    return {
+      url,
+      position: positionOf(i),
+    };
+  });
+
+  const [givenCards, setGivenCards] = useState(() => initialTarotCards);
 
   const cardDrags = useDrag((state) => {
     const atRestWhen = ([vx, vy]: Vector2) => vx === 0 && vy === 0;
@@ -50,6 +67,17 @@ function Dragich() {
         console.log("Dragging paused");
       } else {
         console.log(`Dragging continues at ${state.velocity} px/ms`);
+        console.log(
+          `current position: ${state.xy} | of element`,
+          state.currentTarget
+        );
+
+        const [top, left] = state.xy;
+
+        const [head, ...tail] = givenCards.reverse();
+        const movedHead = { ...head, position: { left, top } };
+
+        setGivenCards([movedHead, ...tail].reverse());
       }
     } else {
       console.log("Drag ended");
@@ -60,12 +88,9 @@ function Dragich() {
 
   return (
     <>
-      {allCards.map((url, i) => (
+      {givenCards.map(({ url, position }, i) => (
         <div className={styles.deck} key={i}>
-          <div
-            {...cardDrags()}
-            style={{ position: "absolute", ...positionOf(i) }}
-          >
+          <div {...cardDrags()} style={{ position: "absolute", ...position }}>
             <Card url={url} />
           </div>
         </div>
